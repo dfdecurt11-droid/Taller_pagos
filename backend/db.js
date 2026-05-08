348a5884c632291e4f4c05f3d46d6478b1690454
dfdecurt11-droid <dfdecurt.11@gmail.com>
2026-05-08 02:03:39 -0500

    Conexión a DB dinámica

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
+// Usamos process.env.DATABASE_URL para que Render se conecte automáticamente
+// Si no existe (en tu PC local), usará los datos que tenías
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Admin_taller', // El nombre que creaste
-    password: 'database',     // Tu contraseña
-    port: 5432,
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:database@localhost:5432/Admin_taller',
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
