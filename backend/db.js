f3b37e55207de4f4854646b4c537a84defdb37ca
dfdecurt11-droid <dfdecurt.11@gmail.com>
2026-05-08 02:25:08 -0500

    Final fix for DB connection string

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } // Render requiere SSL
 });
 
